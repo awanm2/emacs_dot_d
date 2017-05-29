5024757f85e964dd8c49a289688ff6545584e455
mdwen <mhd.ewen@gmail.com>
2017-05-29 11:05:17 -0600

    initial changes to init.el

--- init.el
+++ init.el
@@ -12,15 +12,36 @@
 
 (require 'use-package)
 (setq use-package-always-ensure t)
-
+(require 'function-args) ; package must be installed
+(require 'sr-speedbar)  ;package must be installed
 (add-to-list 'load-path "~/.emacs.d/custom")
-
+;;-----------------------------------------------------
+;; setup helm
+;;info :: a line about helm
+(require 'helm)
+(require 'helm-config)
+(helm-mode 1)
+(helm-autoresize-mode t) ;; auto rize helm buffer
 (require 'setup-general)
+
+(require 'company) ;; package must be installed
+(add-hook 'after-init-hook 'global-company-mode)
+(add-to-list 'company-backends 'company-c-headers)
+
+;;----------------------------------------------------
 (if (version< emacs-version "24.4")
     (require 'setup-ivy-counsel)
   (require 'setup-helm)
   (require 'setup-helm-gtags))
 ;; (require 'setup-ggtags)
+
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode)
+
+
 (require 'setup-cedet)
 (require 'setup-editing)
 
@@ -45,3 +66,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+
+;;--------------------------------------------------------
+(global-set-key (kbd "C-x C-b") 'ibuffer)
